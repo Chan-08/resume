9ddcb3bde32499ab4f62347ad54bb92581339a2f
“[Chandrakumar]” <[itz.chandrakumar@gmail.com]>
2024-02-17 20:01:09 +0530

    new design

--- res.js
+++ res.js
@@ -1,59 +1,103 @@
 
- const projects = [
-    { name: "Resume", description: "Using HTML and CSS", link:"https://github.com/Chan-08/resume" },
-    { name: "Spotify", description: "Cloned version of Spotify", link: "https://github.com/Chan-08/spotify-clone" },
-    { name: "E-Commerce", description: "Model of an e-commerce webpage", link: "https://github.com/Chan-08/E-commerce-webpage" },
-    { name: "E-Learning", description: "E-Learning webpage", link: "https://github.com/Chan-08/E-learning-webpage" }
+
+
+//skills
+
+const programmingSkills = [
+  { name: 'Python', progress: 75 },
+  { name: 'Java', progress: 60 },
+  { name: 'JavaScript', progress: 80 },
+  { name: 'HTML/CSS', progress: 90 }
 ];
 
-const cardsContainer = document.querySelector(".cards-container");
-projects.forEach((project) => {
-const card = document.createElement("div");
-card.classList.add("card");
-card.innerHTML = `
-<ul>
-     <a href="${project.link}">
-                <h2>${project.name}</h2>
-            </a>
-            <p>${project.description}</p>
-        </ul>
-`;
-cardsContainer.appendChild(card);
-});
+const toolsSkills = [
+  { name: 'GitHub', progress: 85 },
+  { name: 'Wordpress', progress: 80 },
+  { name: 'Android Studio', progress: 60 },
+  { name: 'Figma', progress: 80 }
+];
 
-const programmingSkills = ["Java", "Python", "C++", "JavaScript"];
-
-const skillsList = document.querySelector(".skills-list");
-programmingSkills.forEach((skill) => {
-const skillElement = document.createElement("h3");
-skillElement.textContent = skill;
-skillsList.appendChild(skillElement);
-});
-
-function Form() {
-    var name = document.forms["reg"]["name"].value;
-    var email = document.forms["reg"]["email"].value;
-    var num = document.forms["reg"]["num"].value;
-
-    // Check if name is empty
-    if (name == "") {
-        alert("Name must be filled out");
-        return false;
-    }
-
-    // Check if email is empty and in a valid format
-    if (email == "") {
-        alert("Email must be filled out");
-        return false;
-    } 
-
-    // Check if mobile number is empty and contains only digits
-    if (num == "") {
-        alert("Mobile number must be filled out");
-        return false;
-    } 
-
-    // If all checks pass, the form is valid
-    return true;
+function createSkillElement(skill) {
+  return `
+      <div class="skill">
+          <div class="skill-name">${skill.name}</div>
+          <div class="progress-bar">
+              <div class="progress-bar-inner" style="width: ${skill.progress}%;"></div>
+          </div>
+      </div>
+  `;
 }
 
+function populateSkillsContainer(containerId, skills) {
+  const container = document.getElementById(containerId);
+  skills.forEach(skill => {
+      const skillElement = createSkillElement(skill);
+      container.innerHTML += skillElement;
+  });
+}
+
+// Populate programmingSkills and toolsSkills containers
+populateSkillsContainer('programmingSkills', programmingSkills);
+populateSkillsContainer('toolsSkills', toolsSkills);
+
+
+
+
+//projects
+
+const projects = [
+  {
+      name: 'Project 1',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, laboriosam?',
+      imageUrl: 'logo.jpg',
+      githubLink: 'https://github.com/Chan-08'
+  },
+  {
+      name: 'Project 2',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, laboriosam?',
+      imageUrl: 'logo.jpg',
+      githubLink: 'https://github.com/Chan-08'
+  },
+  {
+      name: 'Project 3',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, laboriosam?',
+      imageUrl: 'logo.jpg',
+      githubLink: 'https://github.com/Chan-08'
+  },
+  {
+      name: 'Project 4',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, laboriosam?',
+      imageUrl: 'logo.jpg',
+      githubLink: 'https://github.com/Chan-08'
+  }
+];
+
+const projectsContainer = document.getElementById('projects-container');
+
+projects.forEach(project => {
+  const projectCard = `
+      <div class="card">
+          <img src="${project.imageUrl}" alt="Logo"/>
+          <div class="card-content">
+              <h2>${project.name}</h2>
+              <p>${project.description}</p>
+              <a href="${project.githubLink}" class="button" target="_blank">
+                  View source code
+              </a>
+          </div>
+      </div>
+  `;
+  projectsContainer.innerHTML += projectCard;
+});
+
+
+
+
+
+
+
+
+
+
+
+ 
